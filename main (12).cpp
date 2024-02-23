4ad04e44f6caf316c91336344beb14115d37c5de
Hazeera65 <148674823+Hazeera65@users.noreply.github.com>
2024-02-24 00:55:40 +0530

    Add files via upload

---
+++ main (12).cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    
+    cout << "Enter operator (+, -, *, /,%): ";
+    cin >> operation;
+    switch (operation) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is undefined." << endl;
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                cout << num1 << " % " << num2 << " = " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is undefined." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator." << endl;
+            break;
+    }
+
+    return 0;
+}
+
